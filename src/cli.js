de706e64e7e610ef233367754cc5bb29d4f60d39
alekseyleshko <alekseyleshko@gmail.com>
2014-11-19 16:29:09 +0300

    Fix

--- src/cli.js
+++ src/cli.js
@@ -2,14 +2,14 @@
   'use strict';
 
   function main() {
-  }
+  };
 
   function wrapper() {
     var pckName = 'say-me';
     console.log('Module \'' + pckName + '\' started');
     main();
     console.log('Module \'' + pckName + '\' finished');
-  }
+  };
 
   wrapper();
 })();
