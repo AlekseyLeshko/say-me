181fe7288f3d2587c6cdcc90565828c5ac923357
alekseyleshko <alekseyleshko@gmail.com>
2014-11-21 18:41:13 +0300

    Add help in cli

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,27 @@
 (function() {
   'use strict';
 
+  var optimist = require('optimist');
+  var SayMe = require('./say-me');
+
   function main() {
+    var sayMe = new SayMe();
+
+    var argv = optimist.usage('Usage: say-me <command>', {
+      'help': {
+        description: 'Show help',
+        short: 'h'
+      }
+    })
+    .argv;
+
+    if (argv.h) {
+      optimist.showHelp();
+      process.exit(0);
+    }
+
+    optimist.showHelp();
   };
 
-  function wrapper() {
-    var pckName = 'say-me';
-    console.log('Module \'' + pckName + '\' started');
-    main();
-    console.log('Module \'' + pckName + '\' finished');
-  };
-
-  wrapper();
+  main();
 })();
