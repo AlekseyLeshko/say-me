650f2d61b7ae1c676d45808205ec6ed31240106d
alekseyleshko <alekseyleshko@gmail.com>
2014-12-01 10:50:16 +0300

    Add create script

---
+++ spec/create.spec.js
@@ -0,0 +1,8 @@
+'use strict';
+var sayMe = require('../src/create');
+
+describe('say-me create', function() {
+  it('should exist object', function() {
+    expect(sayMe).toBeDefined();
+  });
+});
