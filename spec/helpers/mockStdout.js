178ab9c1e7c377b4855cbba85877a2277af3eb2d
alekseyleshko <alekseyleshko@gmail.com>
2014-12-05 12:07:16 +0300

    Fix

--- spec/helpers/mockStdout.js
+++ spec/helpers/mockStdout.js
@@ -2,7 +2,17 @@
   'use strict';
 
   function getMockStdout() {
-    var str = '{"dependencies":{"npm":{"version":"2.1.6","from":"npm@","resolved":"https://registry.npmjs.org/npm/-/npm-2.1.6.tgz"}}}';
+    var obj = {
+      dependencies: {
+        npm: {
+          version: '2.1.6',
+          from: 'npm@',
+          resolved: 'https://registry.npmjs.org/npm/-/npm-2.1.6.tgz'
+        }
+      }
+    };
+
+    var str = JSON.stringify(obj);
     return str;
   }
 
